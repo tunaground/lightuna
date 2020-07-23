85e02c16e6dadfa3d9f346a33e84ba3dbe371b56
tunarider <tunarider@tunaground.net>
2020-07-23 09:35:39 +0900

    에러처리 한국어로 수정

---
+++ config/message.php
@@ -0,0 +1,32 @@
+<?php
+const MSG_INVALID_BOARD_UID = '잘못된 게시판 UID입니다.';
+const MSG_INVALID_THREAD_UID = '잘못된 스레드 UID입니다.';
+const MSG_INVALID_INDEX = '잘못된 인덱스입니다.';
+const MSG_INVALID_PASSWORD = '잘못된 패스워드입니다.';
+const MSG_INVALID_FILE = '잘못된 파일입니다.';
+
+const MSG_DIRECTORY_CREATE_FAILED = '디렉토리 생성에 실패했습니다.';
+const MSG_FILE_UPLOAD_FAILED = '파일 업로드에 실패했습니다.';
+const MSG_FILE_MOVE_FAILED = '파일 이동이 실패했습니다.';
+const MSG_FILE_READ_FAILED = '파일 읽기에 실패했습니다.';
+const MSG_FILE_CREATE_FAILED = '파일 생성에 실패했습니다.';
+const MSG_VIRTUAL_IMAGE_CREATE_FAILED = '가상이미지 생성에 실패했습니다.';
+const MSG_IMAGE_RESAMPLE_FAILED = '이미지 샘플링에 실패했습니다.';
+const MSG_IMAGE_SAVE_FAILED = '이미지 저장에 실패했습니다.';
+const MSG_IMAGE_GET_SIZE_FAILED = '이미지 사이즈 획득에 실패했습니다.';
+
+const MSG_CANNOT_DELETE_FIRST_RESPONSE = '0번 리스폰스는 삭제할 수 없습니다.';
+
+const MSG_LIMIT_TITLE_LENGTH = '제목이 너무 깁니다.';
+const MSG_LIMIT_USER_NAME_LENGTH = '유저 이름이 너무 깁니다.';
+const MSG_LIMIT_CONTENT_LENGTH = '본문이 너무 깁니다.';
+const MSG_LIMIT_FILE_TYPE = '잘못된 파일 형식입니다.';
+const MSG_LIMIT_FILE_SIZE = '파일이 너무 큽니다.';
+const MSG_LIMIT_FILENAME_LENGTH = '파일 이름이 너무 깁니다';
+
+const MSG_DENIED_USER = '스레드 관리자에 의해 거부되었습니다.';
+const MSG_THREAD_ENDED = '스레드가 종료되었습니다.';
+const MSG_TOO_MANY_RESPONSE = '리스폰스 요청이 너무 많습니다.';
+const MSG_TOO_MANY_DUPLICATED_RESPONSE = '반복 리스폰스 요청이 너무 많습니다.';
+
+const MSG_QUERY_FAILED = '데이터베이스 질의에 실패했습니다.';
